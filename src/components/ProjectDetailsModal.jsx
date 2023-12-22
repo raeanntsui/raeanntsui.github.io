447649e03e61afe32f6856f2982295d8b3433746
Raeann <raeann.tsui@gmail.com>
2023-12-21 23:00:53 -0800

    semi final portfolio

--- src/components/ProjectDetailsModal.jsx
+++ src/components/ProjectDetailsModal.jsx
@@ -36,14 +36,12 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
         id="exampleModal"
         tabIndex={-1}
         aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
+        aria-hidden="true">
         <div className="modal-dialog modal-xl">
           <div
             className={
               "modal-content " + (darkTheme ? "bg-dark-2 text-light" : "")
-            }
-          >
+            }>
             <div className="modal-body">
               <button
                 type="button"
@@ -55,14 +53,12 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                 className={
                   "container ajax-container " +
                   (darkTheme ? "bg-dark-2 text-light" : "")
-                }
-              >
+                }>
                 <h2
                   className={
                     "text-6 font-weight-600 text-center mb-4" +
                     (darkTheme ? " text-white" : "")
-                  }
-                >
+                  }>
                   {projectDetails?.title}
                 </h2>
                 <div className="row g-4">
@@ -88,8 +84,7 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                       className={
                         "text-4 font-weight-600" +
                         (darkTheme ? " text-white" : "")
-                      }
-                    >
+                      }>
                       Project Info:
                     </h4>
                     <p>{projectDetails?.projectInfo}</p>
@@ -97,16 +92,14 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                       className={
                         "text-4 font-weight-600 mt-4" +
                         (darkTheme ? " text-white" : "")
-                      }
-                    >
+                      }>
                       Project Details:
                     </h4>
                     <ul
                       className={
                         "list-style-2 " + (darkTheme ? "list-style-light" : "")
-                      }
-                    >
-                      <li>
+                      }>
+                      {/* <li>
                         <span
                           className={
                             "text-dark font-weight-600 me-2" +
@@ -116,37 +109,34 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                           Client:
                         </span>
                         {projectDetails?.client}
-                      </li>
+                      </li> */}
                       <li>
                         <span
                           className={
                             "text-dark font-weight-600 me-2" +
                             (darkTheme ? " text-white" : "")
-                          }
-                        >
+                          }>
                           Technologies:
                         </span>
                         {projectDetails?.technologies}
                       </li>
-                      <li>
+                      {/* <li>
                         <span
                           className={
                             "text-dark font-weight-600 me-2" +
                             (darkTheme ? " text-white" : "")
-                          }
-                        >
+                          }>
                           Industry:
                         </span>
                         {projectDetails?.industry}
-                      </li>
+                      </li> */}
                       <li>
                         <span
                           className={
                             "text-dark font-weight-600 me-2" +
                             (darkTheme ? " text-white" : "")
-                          }
-                        >
-                          Date:
+                          }>
+                          Created:
                         </span>
                         {projectDetails?.date}
                       </li>
@@ -155,26 +145,23 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                           className={
                             "text-dark font-weight-600 me-2" +
                             (darkTheme ? " text-white" : "")
-                          }
-                        >
+                          }>
                           URL:
                         </span>
                         <a
                           href={projectDetails?.url?.link}
                           target="_blank"
-                          rel="noopener noreferrer"
-                        >
+                          rel="noopener noreferrer">
                           {projectDetails?.url?.name}
                         </a>
                       </li>
                     </ul>
-                    <div className="row no-gutters align-items-center">
+                    {/* <div className="row no-gutters align-items-center">
                       <div
                         className={
                           "col-auto text-dark font-weight-600" +
                           (darkTheme ? " text-white" : "")
-                        }
-                      >
+                        }>
                         Share:
                       </div>
                       <div className="col-auto">
@@ -182,16 +169,14 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                           className={
                             "social-icons" +
                             (darkTheme ? " social-icons-muted" : "")
-                          }
-                        >
+                          }>
                           <li className="social-icons-facebook">
                             <a
                               data-toggle="tooltip"
                               href={projectDetails?.socialLinks?.facebook}
                               target="_blank"
                               rel="noopener noreferrer"
-                              data-original-title="Facebook"
-                            >
+                              data-original-title="Facebook">
                               <i className="fab fa-facebook-f" />
                             </a>
                           </li>
@@ -201,8 +186,7 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                               href={projectDetails?.socialLinks?.twitter}
                               target="_blank"
                               rel="noopener noreferrer"
-                              data-original-title="Twitter"
-                            >
+                              data-original-title="Twitter">
                               <i className="fab fa-twitter" />
                             </a>
                           </li>
@@ -212,8 +196,7 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                               href={projectDetails?.socialLinks?.google}
                               target="_blank"
                               rel="noopener noreferrer"
-                              data-original-title="Google"
-                            >
+                              data-original-title="Google">
                               <i className="fab fa-google" />
                             </a>
                           </li>
@@ -223,8 +206,7 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                               href={projectDetails?.socialLinks?.instagram}
                               target="_blank"
                               rel="noopener noreferrer"
-                              data-original-title="Instagram"
-                            >
+                              data-original-title="Instagram">
                               <i className="fab fa-instagram" />
                             </a>
                           </li>
@@ -234,14 +216,13 @@ const ProjectDetailsModal = ({ darkTheme, projectDetails }) => {
                               href={projectDetails?.socialLinks?.mail}
                               target="_blank"
                               rel="noopener noreferrer"
-                              data-original-title="Email"
-                            >
+                              data-original-title="Email">
                               <i className="fas fa-envelope" />
                             </a>
                           </li>
                         </ul>
                       </div>
-                    </div>
+                    </div> */}
                   </div>
                 </div>
               </div>
